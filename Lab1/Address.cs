fae05c04917eb8739ba29b70c975fb524d1b0fcd
Ilshat Galimov <einnks@gmail.com>
2015-10-30 00:36:51 +0300

    Ready Lab with classroom task

---
+++ Lab1/Address.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace Lab1
+{
+    public class Address
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int House { get; set; }
+        public int Apartment { get; set; }
+        public int PostCode { get; set; }
+
+        public Address()
+        {
+            Country = "N/A";
+            City = "N/A";
+            Street = "N/A";
+            House = 0;
+            Apartment = 0;
+            PostCode = 0;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(Country + "\n" +
+                              City + "\n" +
+                              Street + "\n" +
+                              House + "\n" +
+                              Apartment + "\n" +
+                              PostCode);
+        }
+
+        public int DisplayMenu()
+        {
+            Console.WriteLine("==========================\n" +
+                              "1. Ввести адресс полностью.\n" +
+                              "2. Вывести адресс.\n" +
+                              "3. Ввести страну.\n" +
+                              "4. Ввести город.\n" +
+                              "5. Ввести улицу.\n" +
+                              "6. Ввести номер дома.\n" +
+                              "7. Ввести номер квартиры.\n" +
+                              "8. Ввести почтовый индекс.\n" +
+                              "9. Выход.\n" +
+                              "==========================\n");
+            var result = Console.ReadLine();
+            return Convert.ToInt32(result);
+        }
+
+    }
+
+    internal class TestAddress
+    {
+        private static void Main()
+        {
+            var address = new Address();
+            int userInput;
+
+            do
+            {
+                userInput = address.DisplayMenu();
+
+                switch (userInput)
+                {
+                    case 1:
+                        Console.Clear();
+                        address.Country = Console.ReadLine();
+                        address.City = Console.ReadLine();
+                        address.Street = Console.ReadLine();
+                        address.House = Convert.ToInt32(Console.ReadLine());
+                        address.Apartment = Convert.ToInt32(Console.ReadLine());
+                        address.PostCode = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.Clear();
+                        address.Print();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        address.Country = Console.ReadLine();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        address.City = Console.ReadLine();
+                        break;
+                    case 5:
+                        Console.Clear();
+                        address.Street = Console.ReadLine();
+                        break;
+                    case 6:
+                        Console.Clear();
+                        address.House = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 7:
+                        Console.Clear();
+                        address.Apartment = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 8:
+                        Console.Clear();
+                        address.PostCode = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 9:
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("ERROR: unexpected number!");
+                        break;
+                }
+            } while (userInput != 9);
+        }
+    }
+}
---
+++ Lab1_classroom/Money.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab1_class
+{
+    class Money
+    {
+        public double Ruble1 { get; set; }
+        public double Ruble2 { get; set; }
+
+        private readonly Dictionary<string, Func<double, double, double>> _operations;
+
+        public Money()
+        {
+            Ruble1 = 0;
+            Ruble2 = 0;
+
+            _operations = new Dictionary<string, Func<double, double, double>>()
+            {
+                { "+", (x, y) => x + y },
+                { "-", (x, y) => x - y },
+                { "/", (x, y) => x / y },
+                { "*", (x, y) => x * y }
+            };
+        }
+
+        public double PerformOperation(string op, double money1, double money2)
+        {
+            if (!_operations.ContainsKey(op))
+                throw new ArgumentException($"Operation {op} is invalid", nameof(op));
+            return _operations[op](money1, money2);
+        }
+
+        public string DisplayMenu()
+        {
+            Console.WriteLine("1. Ввести числа.");
+            Console.WriteLine("2. Ввести операцию.");
+            Console.WriteLine("3. Выход.");
+
+            var cki = Console.ReadKey(false);
+            return cki.KeyChar.ToString();
+        }
+    }
+
+    class TestMoney
+    {
+        private static void Main()
+        {
+            var money = new Money();
+            string userInput;
+
+            do
+            {
+                userInput = money.DisplayMenu();
+
+                switch (userInput)
+                {
+                    case "1":
+                        Console.Clear();
+                        money.Ruble1 = Convert.ToDouble(Console.ReadLine());
+                        money.Ruble2 = Convert.ToDouble(Console.ReadLine());
+                        break;
+                    case "2":
+                        Console.Clear();
+                        try
+                        {
+                            var op = Console.ReadLine();
+                            Console.WriteLine(money.PerformOperation(op, money.Ruble1, money.Ruble2));
+                        }
+                        catch (ArgumentException exception)
+                        {
+                            Console.WriteLine("{0} Exception caught.", exception.Message);
+                            Console.WriteLine("Try to input operation again.");
+                        }
+                        break;
+                    case "3":
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.WriteLine("ERROR: unknown operation!");
+                        break;
+                }
+            } while (userInput != "3");
+        }
+    }
+}
